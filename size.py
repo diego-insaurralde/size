1f49ede988ce7652a7c1b2cc2496f35587c7cd06
diego-insaurralde <diego_insaurralde@live.com>
2023-07-17 10:23:49 -0300

    Add feature to run in differents platforms

--- size.py
+++ size.py
@@ -1,11 +1,14 @@
 import os
 import subprocess
+import platform
 
+
+OS_NAME = platform.system()
 
 
 def calculate_size_in_mb(path):
     total_size = 0
-    
+
     if os.path.isdir(path):
         for root, dirs, files in os.walk(path):
             for file in files:
@@ -13,42 +16,63 @@ def calculate_size_in_mb(path):
                 try:
                     # Get the size of the file in bytes
                     file_size = os.path.getsize(file_path)
-                    # Convert bytes to centimeters (1 byte = 0.000000000000026455 centimeters)
                     file_size_mb = file_size / 1024**2
                     total_size += file_size_mb
-                except OSError:
-                    # Handle file permission errors or other exceptions
-                    pass
-    
+                except Exception as e:
+                    print("Error: ", e)
     else:
         file_size = os.path.getsize(path)
         total_size = file_size / 1024**2
     return total_size
 
-def main(root):
-    list_files = subprocess.run(["dir", "/b" ,root], shell=True, capture_output=True)
 
-    paths = str(list_files.stdout).split("\\r")
-    paths[0] = paths[0].replace("b'","")
-    paths = [path.strip("\\n") for path in paths ]
+def main(root):
+    if OS_NAME == "Windows":
+        slash = "\\"
+        if "powershell" in os.environ.get("SHELL", "").lower():
+            list_files = subprocess.run(["dir", "-name"])
+
+        else:
+            try:
+                list_files = subprocess.run(
+                    ["dir", "/b", root], shell=True, capture_output=True
+                )
+            except Exception as e:
+                print("Error: ", e)
+    elif OS_NAME == "Linux":
+        list_files = subprocess.run(["ls", root], shell=True, capture_output=True)
+        slash = "/"
+
+    paths = list_files.stdout.decode("utf-8").split("\n")
     paths.pop()
     print(paths)
-        
+    if not paths:
+        print("Wrong path or it's empty!")
+        return
 
-    with open(f"{root}\sizes.txt", 'w') as f:
+    len_greater = max(map(len, paths))
+    layout_header = "{:^{width}} | {:^6}"
+    layout_body = "{:^{width}} | {:^6.2f}"
+
+    header = layout_header.format("Paths", "Size (mb)\n", width=len_greater)
+    divider = f'{"-" * (len_greater + 10)}\n'
+
+    with open("sizes.txt", "a") as f:
+        f.write(f"**{root}**\n")
+        f.write(header)
+        f.write(divider)
         for path in paths:
-            print(f"{root}\\{path}")
-            size = calculate_size_in_mb(f"{root}\\{path}")
-            f.write(f"{path:20}------------------ {size:.2f} mb")
-            f.write("\n")
-        
-        
+            print(f"{root}{slash}{path}")
+            size = calculate_size_in_mb(f"{root}{slash}{path}")
+            body = layout_body.format(path, size, width=len_greater)
+            f.write(f"{body}\n")
+
+        f.write(f"{divider}\n")
+
+
 if __name__ == "__main__":
     while True:
         root = input("Path: ")
         if root == "0":
             break
         main(root)
-    
-    
-        
---
+++ test.py
@@ -0,0 +1,8 @@
+a = 40
+
+head = "{:{a}}"
+
+var = head.format("teste", a=a)
+
+
+print(var)
